ae4042636fce0708a91fa69dc637df817ac47c1c
fengling23 <1219777393@qq.com>
2023-01-03 23:33:07 +0800

    for temp save

---
+++ src/com/leetcode/tree/binarytree/DiameterOfBinaryTree_543.java
@@ -0,0 +1,68 @@
+package com.leetcode.tree.binarytree;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DiameterOfBinaryTree_543 {
+
+
+    // 深度是路线上的节点数, 直径是路线上的边数目
+    // 每一条二叉树的「直径」长度，就是一个节点的左右子树的最大深度之和。
+    // 遍历整棵树中的每个节点，然后通过每个节点的左右子树的最大深度算出每个节点的「直径」，最后把所有「直径」求个最大值
+    public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) return 0;
+
+        int rootDiameter = maxDepth(root.left) + maxDepth(root.right);
+        int leftDiameter = diameterOfBinaryTree(root.left);
+        int rightDiameter = diameterOfBinaryTree(root.right);
+        //但是不一定非要经过根节点(比如根节点只有一个右子树, 且右子树两个子树深度和大于右子树)
+        return Math.max(rootDiameter, Math.max(leftDiameter, rightDiameter));
+    }
+
+    //添加map存储高度, 避免重复计算
+    static Map<TreeNode, Integer> map = new HashMap<>();
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (map.containsKey(root)) return map.get(root);
+        int leftHeight = maxDepth(root.left);
+        int rightHeight = maxDepth(root.right);
+        int height = 1 + Math.max(leftHeight, rightHeight);
+        map.put(root, height);
+        return height;
+    }
+
+    public static int diameterOfBinaryTree1(TreeNode root) {
+        int diameter = 0;
+        return maxDepth(root, diameter)[1];
+    }
+
+    //优化: 计算深度的时候一并计算直径
+    public static int[] maxDepth(TreeNode root, int diameter) {
+        if (root == null) return new int[]{0,0};
+
+        int leftHeight = maxDepth(root.left, diameter)[0];
+        int rightHeight = maxDepth(root.right, diameter)[0];
+
+        int height = 0;
+        if (map.containsKey(root))  {
+            height = map.get(root);
+        } else {
+            height = 1 + Math.max(leftHeight, rightHeight);
+            map.put(root, height);
+        }
+
+        diameter = Math.max(diameter, leftHeight+rightHeight);
+
+        return new int[]{height, diameter};
+    }
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(2);
+        System.out.println(diameterOfBinaryTree1(root));
+    }
+
+
+}
